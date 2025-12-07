652b4b2af9a3789aa3675ceb7611e653decaff05
shermidagre <samuelhermida55@gmail.com>
2025-12-07 16:53:10 +0100

    no se levante el modulo despues de los cambios y no se ni porque

--- DockerCompose/DockerComposeExendiendoElModulo/extra-addons/second_proyect_sxe/models/models.py
+++ DockerCompose/DockerComposeExendiendoElModulo/extra-addons/second_proyect_sxe/models/models.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
-
+import datetime
 
 class second_proyect_sxe(models.Model):
 
@@ -12,19 +12,50 @@ class second_proyect_sxe(models.Model):
     edad = fields.Integer(string = "Edad", readonly = True, compute = "_calcular_edad_china", store = True)
     signo_chino = fields.Char(string = "Signo Chino", readonly = True, compute = "_calcular_chinada", store = True)
 
+    codigo_socio = fields.Char(string="Código de Socio")
+
+    nivel_fidelidad = fields.Selection(
+        [
+            ('estandar', 'Estándar'),
+            ('premium', 'Premium'),
+            ('gold', 'Gold')
+        ],
+        string="Nivel de Fidelidad",
+        compute="_compute_nivel_fidelidad",
+        store=True,
+        default='estandar'
+    )
 
     @api.depends('f_nac')
     def _calcular_edad_china(self):
         for record in self:
+            today = datetime.date.today()
             if record.f_nac:
-                record.edad = 130
+                age = today.year - record.f_nac.year
+                record.edad = age
+            else:
+                record.edad = 0
 
     @api.depends('f_nac')
     def _calcular_chinada(self):
         for record in self:
             if record.f_nac:
-                record.signo_chino = "Sin signo"
+                record.signo_chino = "Calculado"
+            else:
+                record.signo_chino = "Sin calcular"
 
+    @api.depends('codigo_socio')
+    def _compute_nivel_fidelidad(self):
+        for record in self:
+            if not record.codigo_socio:
+                # Si el código está vacío
+                record.nivel_fidelidad = 'estandar'
+            elif record.codigo_socio.upper().startswith('G'):
+                # Si el código empieza por "G"
+                record.nivel_fidelidad = 'gold'
+            else:
+                # Si el código tiene valor y no empieza por "G"
+                record.nivel_fidelidad = 'premium'
 
 
 
