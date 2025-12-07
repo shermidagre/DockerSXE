f6ac20b98a259530e382f3bfb163464f56369128
shermidagre <samuelhermida55@gmail.com>
2025-12-07 17:47:49 +0100

    finalizado, ahora a editar readme

--- DockerCompose/DockerComposeExendiendoElModulo/extra-addons/second_proyect_sxe/models/models.py
+++ DockerCompose/DockerComposeExendiendoElModulo/extra-addons/second_proyect_sxe/models/models.py
@@ -1,16 +1,15 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
-import datetime
+from datetime import date
 
 class second_proyect_sxe(models.Model):
-
     _inherit = 'res.partner'
 
     f_nac = fields.Date("Fecha de nacimiento")
 
-    edad = fields.Integer(string = "Edad", readonly = True, compute = "_calcular_edad_china", store = True)
-    signo_chino = fields.Char(string = "Signo Chino", readonly = True, compute = "_calcular_chinada", store = True)
+    edad = fields.Integer(string="Edad", readonly=True, compute="_calcular_edad_china", store=True)
+    signo_chino = fields.Char(string="Signo Chino", readonly=True, compute="_calcular_chinada", store=True)
 
     codigo_socio = fields.Char(string="Código de Socio")
 
@@ -29,33 +28,31 @@ class second_proyect_sxe(models.Model):
     @api.depends('f_nac')
     def _calcular_edad_china(self):
         for record in self:
-            today = datetime.date.today()
             if record.f_nac:
-                age = today.year - record.f_nac.year
+                today = date.today()
+                # Fórmula precisa para edad teniendo en cuenta si ya cumplió años o no
+                age = today.year - record.f_nac.year - ((today.month, today.day) < (record.f_nac.month, record.f_nac.day))
                 record.edad = age
             else:
                 record.edad = 0
 
     @api.depends('f_nac')
     def _calcular_chinada(self):
+        # Lista ordenada según el resto de dividir año / 12
+        zodiacos = ['Mono', 'Gallo', 'Perro', 'Cerdo', 'Rata', 'Buey', 'Tigre', 'Conejo', 'Dragón', 'Serpiente', 'Caballo', 'Cabra']
         for record in self:
             if record.f_nac:
-                record.signo_chino = "Calculado"
+                indice = record.f_nac.year % 12
+                record.signo_chino = zodiacos[indice]
             else:
-                record.signo_chino = "Sin calcular"
+                record.signo_chino = "Desconocido"
 
     @api.depends('codigo_socio')
     def _compute_nivel_fidelidad(self):
         for record in self:
             if not record.codigo_socio:
-                # Si el código está vacío
                 record.nivel_fidelidad = 'estandar'
             elif record.codigo_socio.upper().startswith('G'):
-                # Si el código empieza por "G"
                 record.nivel_fidelidad = 'gold'
             else:
-                # Si el código tiene valor y no empieza por "G"
                 record.nivel_fidelidad = 'premium'
-
-
-
